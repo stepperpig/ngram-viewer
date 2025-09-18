31ba381f170ee8f664f83ff640bf1a044de373bf
stepperpig <stepperpig@gmail.com>
2025-09-18 11:22:19 -0700

    it basically functions...

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,10 +16,10 @@ public class Main {
     public static void main(String[] args) {
         NgordnetServer hns = new NgordnetServer();
 
-        NGramMap ngm = new NGramMap(SHORT_WORDS_FILE, TOTAL_COUNTS_FILE);
+        NGramMap ngm = new NGramMap(Q_WORDS_FILE, TOTAL_COUNTS_FILE);
 
         hns.startUp();
-        hns.register("history", new DummyHistoryHandler());
+        hns.register("history", new HistoryHandler(ngm));
         hns.register("historytext", new HistoryTextHandler(ngm));
 
         System.out.println("Finished server startup! Visit http://localhost:4567/ngordnet_2a.html");
